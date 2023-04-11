dafa9c2c706dc04bbb7b7079d6897190979f58f1
Mary Jane Santos <120022066+msmaryjanesantos@users.noreply.github.com>
2023-04-11 19:33:12 +0800

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,8 @@
+//1. Select the "Add to Cart" button
+//2. Select the cart
+//3. Initialize the cart total
+//4. Add a click event listener to the "Add to Cart" button
+//5. Get the product information
+//6. Create a new cart item
+//7. Add the cart item to the cart
+//8. Update the cart total
